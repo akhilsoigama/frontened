125649796c3eb19dade5a6740c80adfe61754f8b
akhil soigama <akhilsoigama@gmail.com>
2024-08-21 07:46:46 +0530

    changes in files

---
+++ BOM/script.js
@@ -0,0 +1,7 @@
+// innerwidth and innerHeight
+let demo1 = document.querySelector("#demo1");
+demo1.innerHTML = "browser inne window width:"+window.innerWidth + "px<br>"+"browser inne window height:"+window.innerHeight+"px";
+
+// screen width
+let demo2 = document.querySelector("#demo2");
+demo2.innerHTML = "Screen width is" + screen.width + "px";
