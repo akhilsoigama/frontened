086675c766d8f09c8d7eb15be830c96795d733b8
akhil soigama <akhilsoigama@gmail.com>
2024-09-14 09:04:29 +0530

    password button add

---
+++ drop-img/script.js
@@ -0,0 +1,30 @@
+let imgBox2 = document.querySelector(".img-box2");
+
+let boxes = document.getElementsByClassName("box");
+
+imgBox2.addEventListener("dragstart", (e) => {
+    e.target.className += ' hold';
+    setTimeout(() => {
+        e.target.className = ' hide';
+    }, 0);
+});
+imgBox2.addEventListener("dragend", (e) => {
+    e.target.className = ' img-box2';
+});
+
+
+for (box of boxes) {
+    box.addEventListener("dragover", (e) => {
+        e.preventDefault();
+    })
+    box.addEventListener("dragenter", (e) => {
+        e.target.className += " dashed"
+    })
+    box.addEventListener("dragleave", (e) => {
+        e.target.className = 'box';
+    })
+    box.addEventListener("drop", (e) => {
+        e.target.append(imgBox2);
+
+    })
+}
