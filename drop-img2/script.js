d3dab3b26e25c00e44dbe156006a49940bd115e4
akhil soigama <akhilsoigama@gmail.com>
2024-09-16 09:13:20 +0530

    drag and drop img

--- drop-img2/script.js
+++ drop-img2/script.js
@@ -1,19 +1,19 @@
-let input = document.getElementById("file-input");
-let dropImg = document.getElementById("drop-img");
-let imgView = document.getElementById("img-view");
+let input = document.getElementById("choose-img");
+let dropArea = document.getElementById("drop-area");
+let imgBox = document.getElementById("img-box");
 
 input.addEventListener("change", uploadImage);
 
 function uploadImage() {
-    let url = URL.createObjectURL(input.files[0]);
-    imgView.style.backgroundImage = `URL(${url})`;
-    imgView.textContent = "";
-    imgView.style.border = "none";
+    let urlLink = URL.createObjectURL(input.files[0]);
+    imgBox.style.backgroundImage = `URL(${urlLink})`;
+    imgBox.textContent = "";
+    imgBox.style.border = "none";
 }
-dropImg.addEventListener("dragover", (e) => {
+dropArea.addEventListener("dragover", (e) => {
     e.preventDefault();
 });
-dropImg.addEventListener("drop", (e) => {
+dropArea.addEventListener("drop", (e) => {
     e.preventDefault();
     input.files = e.dataTransfer.files;
     console.log(input.files);
