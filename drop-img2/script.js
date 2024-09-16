45135c0d6b9c15f47fe07d0548ad310764053f13
akhil soigama <akhilsoigama@gmail.com>
2024-09-16 09:16:02 +0530

    drag event

--- drop-img2/script.js
+++ drop-img2/script.js
@@ -2,9 +2,11 @@ let input = document.getElementById("choose-img");
 let dropArea = document.getElementById("drop-area");
 let imgBox = document.getElementById("img-box");
 
-input.addEventListener("change", uploadImage);
+input.addEventListener("change", function chooseImg (){
 
-function uploadImage() {
+});
+
+function chooseImg() {
     let urlLink = URL.createObjectURL(input.files[0]);
     imgBox.style.backgroundImage = `URL(${urlLink})`;
     imgBox.textContent = "";
@@ -17,5 +19,5 @@ dropArea.addEventListener("drop", (e) => {
     e.preventDefault();
     input.files = e.dataTransfer.files;
     console.log(input.files);
-    uploadImage();
+    chooseImg();
 })
